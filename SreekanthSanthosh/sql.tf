c4f8052b88a94d0f93c2fb547be59412c696e866
sreekanthsanthosh <96111593+sreekanthsanthosh@users.noreply.github.com>
2021-12-14 18:39:20 +0530

    Add files via upload

---
+++ SreekanthSanthosh/sql.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  credentials = "tarkshyas-1636464440135-f16454fda337.json"
+  project     = "tarkshyas-1636464440135"
+  region      = "us-central1"
+}
---
+++ SreekanthSanthosh/vm.tf
@@ -0,0 +1,6 @@
+provider "google" {
+  credentials= "tarkshyas-1636464440135-f16454fda337.json"
+  project    = "tarkshyas-1636464440135"
+  region     = "us-central1"
+  zone       = "us-central1-c"
+}
